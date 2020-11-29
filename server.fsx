04abf882eff415ef7af169bdc571515ec39c95a6
vasu-negi <vasu.negi1995@gmail.com>
2020-11-29 13:45:05 -0500

    commit

--- server.fsx
+++ server.fsx
@@ -9,3 +9,86 @@ open System.Text
 open System.Collections.Generic
 open System.Globalization
 
+type User ={
+    Username: String
+    Password: String
+    Feeds: int[]
+    
+}
+
+let Server (mailbox: Actor<_>) =
+    let rec loop() = actor {
+        let! msg = mailbox.Receive()
+        match msg with 
+
+        return! loop()
+    }
+    loop()
+
+let User (mailbox: Actor<_>) =
+
+    let rec loop() = actor {
+        let! msg = mailbox.Receive()
+        match msg with 
+        return! loop()
+    }
+    loop()
+
+let HashTagFunction (mailbox: Actor<_>) =
+
+    let rec loop() = actor {
+        let! msg = mailbox.Receive()
+        match msg with 
+        return! loop()
+    }
+    loop()
+
+
+let MentionsFunction (mailbox: Actor<_>) =
+
+    let rec loop() = actor {
+        let! msg = mailbox.Receive()
+        match msg with 
+        return! loop()
+    }
+    loop()
+
+
+let UpdateDatabase (mailbox: Actor<_>) =
+
+    let rec loop() = actor {
+        let! msg = mailbox.Receive()
+        match msg with 
+        return! loop()
+    }
+    loop()
+
+
+let PushTweets (mailbox: Actor<_>) =
+
+    let rec loop() = actor {
+        let! msg = mailbox.Receive()
+        match msg with 
+        return! loop()
+    }
+    loop()
+
+
+let Login (mailbox: Actor<_>) =
+
+    let rec loop() = actor {
+        let! msg = mailbox.Receive()
+        match msg with 
+        return! loop()
+    }
+    loop()
+
+
+let Logout (mailbox: Actor<_>) =
+
+    let rec loop() = actor {
+        let! msg = mailbox.Receive()
+        match msg with 
+        return! loop()
+    }
+    loop()
